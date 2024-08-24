a1661efb902e64305f19917cf5588620abd75bcd
Sahin3892 <75809365+Sahin3892@users.noreply.github.com>
2024-08-24 23:04:50 +0200

    Added Collision Detection

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -29,6 +29,20 @@ class MovableObject {
         this.img.src = path;
     }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        if (this instanceof Character || this instanceof Goblin) {
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+    }
+
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
