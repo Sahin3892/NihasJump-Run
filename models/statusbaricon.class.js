31ed30477330d3820c02af55a51a88aa435f0470
Sahin <sahin.duruoez@gmx.de>
2024-09-03 15:54:18 +0200

    changed  x&y on Icon

--- models/statusbaricon.class.js
+++ models/statusbaricon.class.js
@@ -3,8 +3,8 @@ class Statusbaricon extends DrawableObject {
     constructor() {
         super();
         this.loadImage('src/img/AVAS/ava2.png');
-        this.x = 28;
-        this.y = 27;
+        this.x = 27.5;
+        this.y = 27.5;
         this.width = 65;
         this.height = 65;
     }
