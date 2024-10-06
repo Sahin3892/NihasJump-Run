5265b8f74db0f55bad1ff8e909d95cf378fbf1ef
Sahin <sahin.duruoez@gmx.de>
2024-10-06 19:26:01 +0200

    Update goblin.class.js

--- models/goblin.class.js
+++ models/goblin.class.js
@@ -18,6 +18,7 @@ class Goblin extends MovableObject {
     ];
 
     goblin_sound = new Audio('src/audio/orcgaunt1.mp3');
+    goblin_sound_death = new Audio('src/audio/Goblin2.mp3');
     offset = {
         top: 65,
         left: 70,
@@ -43,19 +44,20 @@ class Goblin extends MovableObject {
             this.otherDirection = false;
         }, 1000 / 60);
         setInterval(() => {
-              if(this.isDead) {
+            if (this.isDead) {
                 this.playAnimation(this.IMAGES_DEAD);
-                  if(this.currentImage === 4) {
-                      // Hier ist alles zu Ende
-                      // Alle Animationen anhalten
-                      // Goblin löschen
-                  }
-              } else {
-                    this.playAnimation(this.IMAGES_WALKING);
-                    this.goblinScream();
+                if (this.currentImage === 4) {
+                    // Hier ist alles zu Ende
+                    // Alle Animationen anhalten
+                    // Goblin löschen
                 }
+            } else {
+                this.playAnimation(this.IMAGES_WALKING);
+                //  this.goblinScream();
+            }
         }, 300);
     }
+
     die(enemies, enemyIndex) {
         if (!this.isDead) {
             this.currentImage = 0;
@@ -68,7 +70,7 @@ class Goblin extends MovableObject {
     }
 
     goblinScream() {
-        //   this.goblin_sound.play();
-        //   this.goblin_sound.volume = 0.05;
+        this.goblin_sound.play();
+        this.goblin_sound.volume = 0.05;
     }
 }
