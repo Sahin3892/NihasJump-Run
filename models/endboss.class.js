7e958d6515eba6e55dd1d2173555053a2e17db30
Sahin <sahin.duruoez@gmx.de>
2024-12-01 19:17:14 +0100

    Added Images Hurt.
    And Collision Detection

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -10,17 +10,23 @@ class Endboss extends MovableObject {
         'src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Anger4.png',
         'src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Anger5.png'
     ];
+
+    IMAGES_HURT = [
+        'src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Hurt1.png',
+        'src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Hurt2.png',
+    ];
     offset = {
-        top: 170, // Y
-        left: 215, // X
-        right: 215, // width
-        bottom: 140 // height
+        top: 170,
+        left: 215,
+        right: 215,
+        bottom: 140
     };
 
     constructor() {
         super().loadImage('src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Anger1.png');
         this.loadImages(this.IMAGES_WALKING);
         this.x = 2350;
+        this.health = 100; // Beispielwert für die Lebenspunkte
         this.animate();
     }
 
@@ -28,5 +34,17 @@ class Endboss extends MovableObject {
         setInterval(() => {
             this.playAnimation(this.IMAGES_WALKING);
         }, 200);
+    }
+
+    takeDamage(amount) {
+        this.health -= amount;
+        if (this.health <= 0) {
+            this.die();
+        }
+    }
+
+    die() {
+        // Logik für das Sterben des Bosses, z.B. Animation oder Spielstatus ändern
+        console.log("Der Boss ist besiegt!");
     }
 }
