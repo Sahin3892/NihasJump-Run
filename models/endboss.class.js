c3f7ca07455090af882cc00fc0fd969a7f1b8103
Sahin <sahin.duruoez@gmx.de>
2025-02-14 14:37:15 +0100

    Added Boss Dead Image

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -18,6 +18,14 @@ class Endboss extends MovableObject {
         'src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Hurt2.png'
     ];
 
+    IMAGES_DEAD_BOSS = [
+        'src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Death0.png',
+        'src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Death1.png',
+        'src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Death2.png',
+        'src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Death3.png',
+        'src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Death4.png'
+    ];
+
     offset = {
         top: 170,
         left: 215,
@@ -37,22 +45,27 @@ class Endboss extends MovableObject {
         const actualDamage = Math.min(damage, 20); // Ensure damage does not exceed 20
         this.health -= actualDamage; // Decrease health by actual damage
         this.percentage = this.health; // Update percentage for health bar
+        clearInterval(this.walkingInterval); // Stop walking animation
         this.playAnimation(this.IMAGES_HURT); // Play hurt animation
         if (this.health <= 0) {
             this.health = 0;
             this.percentage = 0;
             this.bossDie();
+        } else {
+            setTimeout(() => {
+                this.animate(); // Resume walking animation after hurt animation
+            }, 500); // Adjust the timeout duration as needed
         }
+    }
+
+    animate() {
+        this.walkingInterval = setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 200);
     }
 
     bossDie() {
         // Implement boss death logic here
         console.log('Boss is dead');
-    }
-
-    animate() {
-        setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
-        }, 200);
     }
 }
