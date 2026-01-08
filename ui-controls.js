6e0c6b1326be3b5cb90ee276b0acb45513c43a3a
Sahin <sahin.duruoez@gmx.de>
2026-01-08 18:28:48 +0100

    Add fullscreen support for game canvas
    Introduced fullscreen functionality by adding a new ui-controls.js script, updating index.html to wrap the canvas in a fullscreen div and include the new script, and adding fullscreen-specific CSS. Also added a fullscreen icon asset.

---
+++ ui-controls.js
@@ -0,0 +1,24 @@
+function fullscreen() {
+  let fullscreen = document.getElementById("fullscreen");
+  enterFullscreen(fullscreen);
+}
+
+function enterFullscreen(element) {
+  var element = document.getElementById("canvas");
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.msRequestFullscreen) {      // for IE11 (remove June 15, 2022)
+    element.msRequestFullscreen();
+  } else if(element.webkitRequestFullscreen) {  // iOS Safari
+    element.webkitRequestFullscreen();
+  }
+}
+
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
