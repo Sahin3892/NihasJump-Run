d0d45c1361d56fb1a7d01a9adf24c1dd4c43bec5
Sahin <sahin.duruoez@gmx.de>
2026-01-19 21:09:34 +0100

    Update character.class.js

--- models/character.class.js
+++ models/character.class.js
@@ -220,3 +220,25 @@ class Character extends MovableObject {
     }, 100);
   }
 }
+
+
+
+// Florians Lösung vom LiveCall
+//  deathAnimationComplete = false;
+//  deathIndex = 0;
+// CharDieAnimation() {
+//    if (this.deathAnimationComplete === false) {
+//      
+//      this.playAnimation(this.IMAGES_DEAD); // Spiele Animation
+//      // Prüfe ob jetzt beim letzten Bild ist
+//      // this.dead_sound_boss.play();
+//      this.deathIndex++;
+//      if (this.deathIndex >= this.IMAGES_DEAD.length) {
+//        this.deathAnimationComplete = true;
+//        // Funktion clearAllInterval()        
+//      }
+//    }
+//    clearTimeout(this.idleTimer);
+//    clearInterval(this.idleInterval);
+//    console.log(this.currentImage);
+//  }
