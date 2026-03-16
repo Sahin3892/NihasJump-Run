3a238b69b67f2a0f873a67814475b0074db26413
Sahin <sahin.duruoez@gmx.de>
2026-03-16 18:36:02 +0100

    Add BossFireBall class and assets
    Add a new BossFireBall class file with an IMAGES_BOSS_FIRE sprite array and a placeholder constructor. Expose the new script in index.html. Also add the same IMAGES_BOSS_FIRE array to the Endboss class to prepare for boss fireball animations/behavior. Implementation of movement/animation/interaction remains to be completed.

---
+++ models/bossFireBall.class.js
@@ -0,0 +1,19 @@
+class BossFireBall extends MovableObject {
+    
+  IMAGES_BOSS_FIRE = [
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire1.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire2.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire3.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire4.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire5.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire6.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire7.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire8.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire9.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire10.png",
+  ];
+  
+  constructor(x, y, direction, speedY, speedX) {
+
+  }
+}
--- models/endboss.class.js
+++ models/endboss.class.js
@@ -43,6 +43,19 @@ class Endboss extends MovableObject {
     "src/img/bosses-pixel-art-game-assets-pack/PNG/Boss3/Anger5.png",
   ];
 
+  IMAGES_BOSS_FIRE = [
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire1.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire2.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire3.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire4.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire5.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire6.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire7.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire8.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire9.png",
+    "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire10.png",
+  ];
+
   hadFirstContact = false;
   i = 0;
   damage_sound_boss = new Audio("src/audio/boss_hit_sound.mp3");
