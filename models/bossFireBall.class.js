9a64c586042bf17797a1caf70136999171270bc9
Sahin <sahin.duruoez@gmx.de>
2026-03-17 21:19:13 +0100

    Refactor BossFireBall constructor for targeting
    Change BossFireBall to support targeted casting: add castPoint, targetX/targetY and an offset object; update constructor signature to (x, y, targetX, targetY); load initial image assets with loadImage/loadImages and set fixed sprite size (128x128). Removes previous direction/speed parameters in favor of explicit target coordinates. Note: the current assignments use this.targetX = this.targetX and this.targetY = this.targetY, which appear incorrect and should be changed to this.targetX = targetX and this.targetY = targetY.

--- models/bossFireBall.class.js
+++ models/bossFireBall.class.js
@@ -1,5 +1,4 @@
 class BossFireBall extends MovableObject {
-    
   IMAGES_BOSS_FIRE = [
     "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire1.png",
     "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire2.png",
@@ -12,8 +11,27 @@ class BossFireBall extends MovableObject {
     "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire9.png",
     "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire10.png",
   ];
-  
-  constructor(x, y, direction, speedY, speedX) {
+  castPoint;
+  targetX;
+  targetY;
+  offset = {
+    top: 10,
+    left: 10,
+    right: 10,
+    bottom: 10,
+  };
 
+  constructor(x, y, targetX, targetY) {
+    super();
+    this.loadImage(
+      "src/img/bosses-pixel-art-game-assets-pack/PNG/Magic_Attacks/fire1.png",
+    );
+    this.loadImages(this.IMAGES_BOSS_FIRE);
+    this.x = x;
+    this.y = y;
+    this.targetX = this.targetX;
+    this.targetY = this.targetY;
+    this.height = 128;
+    this.width = 128;
   }
 }
